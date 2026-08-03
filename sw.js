6bb258ac32d2b75d316928306d17e55650d78867
tomitaro25 <132456822+tomitaro25@users.noreply.github.com>
2026-08-03 17:13:03 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,9 +1,11 @@
-const CACHE_NAME = 'karteikarten-v18';
+const CACHE_NAME = 'karteikarten-v19';
 const APP_SHELL = [
   './index.html',
   './manifest.json',
   './icon-192.png',
   './icon-512.png',
+  './icon-192-maskable.png',
+  './icon-512-maskable.png',
   './vocab-data.js'
 ];
 
